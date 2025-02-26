44a8cec4dd40852b18979515dc3ec3da30e6d996
to-kubota <to-kubota@kddi.com>
2025-02-26 18:35:19 +0900

    Resource修正

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -7,10 +7,10 @@ export const storage = defineStorage({
       allow.guest.to(["read"]),
       allow.authenticated.to(["read", "write", "delete"]),
     ],
-    "protected/{entity_id}/*": [
-      allow.authenticated.to(["read"]),
-      allow.entity("identity").to(["read", "write", "delete"]),
-    ],
+    // "protected/{entity_id}/*": [
+    //   allow.authenticated.to(["read"]),
+    //   allow.entity("identity").to(["read", "write", "delete"]),
+    // ],
     "private/{entity_id}/*": [
       allow.entity("identity").to(["read", "write", "delete"]),
     ],
--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import {
 } from "@aws-amplify/ui-react-storage/browser";
 import "@aws-amplify/ui-react-storage/styles.css";
 // import type { Schema } from "../amplify/data/resource";
-// import { useAuthenticator } from '@aws-amplify/ui-react';
+import { useAuthenticator } from '@aws-amplify/ui-react';
 // import { generateClient } from "aws-amplify/data";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
@@ -18,7 +18,6 @@ export const { StorageBrowser } = createStorageBrowser({
 // const client = generateClient<Schema>();
 
 // function App() {
-//   const { user, signOut } = useAuthenticator();
 //   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
 //   useEffect(() => {
@@ -35,9 +34,25 @@ export const { StorageBrowser } = createStorageBrowser({
 //     client.models.Todo.delete({ id })
 //   }
 function App() {
+  const { signOut } = useAuthenticator();
   return (
     <>
+      <button onClick={signOut}
+      style={{
+        position: "absolute",
+        top: "10px",
+        right: "10px",
+        padding: "8px 16px",
+        backgroundColor: "#007bff",
+        color: "#fff",
+        border: "none",
+        borderRadius: "5px",
+        cursor: "pointer",
+      }}
+      >Sign out</button>
+      <div style={{ marginTop: "50px" }}>
       <StorageBrowser />
+      </div>
     </>
   );
 }
